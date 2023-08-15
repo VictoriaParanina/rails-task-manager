4961cf64484e4293de921e25f0e4434f4dae2ae3
Victoria Paranina <paranina.victoria@gmail.com>
2023-08-15 15:14:35 +0200

    done no design no opti

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,4 +20,20 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to home_path(@task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path, status: :see_other
+  end
 end
