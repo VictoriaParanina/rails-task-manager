ee0558c279109a73faf1f0e95d67ba9650879934
Victoria Paranina <paranina.victoria@gmail.com>
2023-08-15 16:17:16 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: "tasks#index", as: :home
-  get "tasks/new", to: "tasks#new", as: :new
-  get "tasks/:id", to: "tasks#show", as: :task
-  post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: 'tasks#edit', as: :edit
-  patch "tasks/:id", to:"tasks#update", as: :update
-  delete "tasks/:id", to: "tasks#destroy", as: :delete
+  # get "tasks", to: "tasks#index", as: :home
+  # get "tasks/new", to: "tasks#new", as: :new
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: 'tasks#edit', as: :edit
+  # patch "tasks/:id", to:"tasks#update", as: :update
+  # delete "tasks/:id", to: "tasks#destroy", as: :delete
+  resources :tasks
 end
